4c5f18df9559743ee9525464406277e194aa4b6e
Harm Meijer <harm.meijer@commercetools.de>
2020-09-01 12:58:38 +0200

    with cleanup script

---
+++ scripts/cleanup/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs-extra');
+const glob = require('glob');
+const { exit } = require('process');
+
+const filter = (arr) => {
+  const recursiveFilter = (arr, index)=>{
+    if (arr.length === index - 1) {
+      return arr;
+    }
+    const currentItem = arr[index];
+    const newArray = arr
+      .slice(0, index + 1)
+      .concat(
+        arr.slice(index + 1).filter(
+          (p)=>
+            p.indexOf(currentItem) !== 0
+        )
+      );
+    return recursiveFilter(newArray, index + 1);
+  };
+  return recursiveFilter(arr, 0);
+};
+
+glob('**/node_modules/', (er, files) => {
+  if (er) {
+    console.error(er);
+    exit(1);
+  }
+  filter(files).forEach(
+    dir=>
+      fs.remove(dir, { recursive: true }, (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log(`${dir} is deleted!`);
+      })
+  );
+});
